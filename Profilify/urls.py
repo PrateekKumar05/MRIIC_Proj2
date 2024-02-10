06fcb1e00ab1bb6271a4e3e8e8adab6a2c935458
Prateek Kumar <prateekptp@gmail.com>
2024-02-10 11:17:34 +0530

    added user login and registration functionalities

--- Profilify/urls.py
+++ Profilify/urls.py
@@ -21,4 +21,8 @@ from RPMS import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('loginuser', views.loginuser, name='loginuser'),
+    path('registration', views.registration, name='registration'),
+    path('logoutuser', views.logoutuser, name='logoutuser'),
+    path('aboutus', views.aboutus, name='aboutus'),
 ]
--- RPMS/views.py
+++ RPMS/views.py
@@ -1,6 +1,51 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def home(request):
     return render(request, 'RPMS/home.html')
     # return HttpResponse('Home Page')
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'RPMS/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        a = request.POST.get('username')
+        b = request.POST.get('password')
+        user = authenticate(request, username=a, password=b)
+        if user is None:
+            return render(request,'RPMS/loginuser.html', {'form':AuthenticationForm(), 'error':'Invalid Credentials'})
+        else:
+            login(request, user)
+            return redirect('home')
+
+
+def registration(request):
+        if request.method == 'GET':
+            return render(request, 'RPMS/registration.html', {'form':UserCreationForm()})
+        else:
+            a = request.POST.get('username')
+            b = request.POST.get('password1')
+            c = request.POST.get('password2')
+            if b==c:
+                #Check whether user name is unique
+                if (User.objects.filter(username = a)):
+                    return render(request, 'RPMS/registration.html', {'error':'Username Already Exists Try again with different username'})
+                else:
+                    user = User.objects.create_user(username = a, password=b)
+                    user.save()
+                    login(request,user)
+                    return redirect('home')
+            else:
+                return render(request, 'RPMS/registration.html', {'form':UserCreationForm(), 'error':'Password Mismatch Try Again'})
+        
+def logoutuser(request):
+    if request.method == 'GET':
+        logout(request)
+        return redirect('home')
+    
+def aboutus(request):
+    return render(request, 'RPMS/aboutus.html')
