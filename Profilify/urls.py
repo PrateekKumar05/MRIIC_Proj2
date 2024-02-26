788443682815b06a0979f6c264961945b9788876
Prateek Kumar <prateekptp@gmail.com>
2024-02-27 00:08:54 +0530

    Added leaderboard and logistics page

--- Profilify/urls.py
+++ Profilify/urls.py
@@ -4,6 +4,10 @@ from RPMS import views
 from RPMS.views import upload_file
 from django.conf import settings
 from django.conf.urls.static import static
+from RPMS.views import leaderboard
+from RPMS.views import chart_data
+from RPMS.views import logistics
+from RPMS.views import download_contact_info
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +19,8 @@ urlpatterns = [
     path('contact',views.contact,name='contact'),
     path('upload/', views.upload_file, name='upload_file'),
     path('profile', views.profile, name='profile'),
+    path('leaderboard/', leaderboard, name='leaderboard'),
+    path('chart-data/', chart_data, name='chart_data'),
+    path('logistics/', logistics, name='logistics'),
+    path('download-contact-info/', download_contact_info, name='download_contact_info'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- RPMS/models.py
+++ RPMS/models.py
@@ -10,6 +10,7 @@ class UserProfile(models.Model):
     education = models.CharField(max_length=100, default=True)
     research_interests = models.CharField(max_length=255, default=True)
     profile_photo = models.ImageField(upload_to='profile_photo/', blank=True, null=True)
+    points = models.IntegerField(default=0)
 
     def __str__(self):
         return f'{self.user}'
